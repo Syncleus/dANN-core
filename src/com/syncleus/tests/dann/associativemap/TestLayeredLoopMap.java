b93ab7d3fee00da82c9b73832c6ec79cd5f8761a
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2009-05-24 00:08:35 +0000

    Removed some code no longer used due to the last commit.
    git-svn-id: svn://svn.syncleus.com/dANN/trunk@195 6ae8b97b-f314-0410-8212-aecf10b92ded

--- src/com/syncleus/tests/dann/associativemap/TestLayeredLoopMap.java
+++ src/com/syncleus/tests/dann/associativemap/TestLayeredLoopMap.java
@@ -20,43 +20,7 @@ public class TestLayeredLoopMap
 	{
 		testMap = null;
 	}
-/*
-	@Test
-	public void testLayeredLoopIndividual()
-	{
 
-		AssociativeNode[][] nodes = testMap.getLayers();
-
-		//find the farthest nodes in layer 1 and 2
-		double farthestAdjacent = 0.0;
-		double closestSeperated = Double.MAX_VALUE;
-		for(int primaryLayerIndex = 0; primaryLayerIndex < nodes[0].length; primaryLayerIndex++)
-		{
-			AssociativeNode currentPrimaryLayerNode = nodes[0][primaryLayerIndex];
-
-			for(int adjacentLayerIndex = 0; adjacentLayerIndex < nodes[1].length; adjacentLayerIndex++)
-			{
-				AssociativeNode currentAdjacentLayerNode = nodes[1][adjacentLayerIndex];
-				double currentDistance = currentPrimaryLayerNode.getLocation().calculateRelativeTo(currentAdjacentLayerNode.getLocation()).getDistance();
-
-				if(farthestAdjacent < currentDistance)
-					farthestAdjacent = currentDistance;
-			}
-
-
-			for(int seperatedLayerIndex = 0; seperatedLayerIndex < nodes[16].length; seperatedLayerIndex++)
-			{
-				AssociativeNode currentSeperatedLayerNode = nodes[16][seperatedLayerIndex];
-				double currentDistance = currentPrimaryLayerNode.getLocation().calculateRelativeTo(currentSeperatedLayerNode.getLocation()).getDistance();
-
-				if(closestSeperated > currentDistance)
-					closestSeperated = currentDistance;
-			}
-		}
-
-		Assert.assertTrue("Associative Map did not properly align: farthestAdjacent:" + farthestAdjacent + " closestSeperated:" + closestSeperated, farthestAdjacent < closestSeperated);
-	}
-*/
 	@Test
 	public void testLayeredLoopAverage()
 	{
