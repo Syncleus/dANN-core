d1e49709f9ff0a1bc6d4e85027633be7b578d66e
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2009-08-01 08:38:41 +0000

    Made hyperassociative map multi-threaded.
    git-svn-id: svn://svn.syncleus.com/dANN/trunk@376 6ae8b97b-f314-0410-8212-aecf10b92ded

--- src/com/syncleus/dann/graph/hyperassociativemap/AbstractHyperassociativeMap.java
+++ src/com/syncleus/dann/graph/hyperassociativemap/AbstractHyperassociativeMap.java
@@ -20,6 +20,12 @@ package com.syncleus.dann.graph.hyperassociativemap;
 import com.syncleus.dann.math.Hyperpoint;
 import java.io.Serializable;
 import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -38,7 +44,39 @@ public abstract class AbstractHyperassociativeMap implements Serializable
 	 * @since 1.0
 	 */
     protected HashSet<HyperassociativeNode> nodes = new HashSet<HyperassociativeNode>();
+
 	private int dimensions;
+	private ThreadPoolExecutor threadExecutor;
+
+	private class Align implements Callable<Hyperpoint>
+	{
+		private HyperassociativeNode node;
+
+		public Align(HyperassociativeNode node)
+		{
+			this.node = node;
+		}
+
+		public Hyperpoint call()
+		{
+			this.node.align();
+			return this.node.getLocation();
+		}
+	}
+
+	/**
+	 * Initializes a HyperassociativeMap of the specified dimensions.
+	 *
+	 *
+	 * @param dimensions The number of dimensions for this map.
+	 * @param threadExecutor the threadExecutor used to manage threads.
+	 * @since 1.0
+	 */
+	public AbstractHyperassociativeMap(int dimensions, ThreadPoolExecutor threadExecutor)
+	{
+		this.dimensions = dimensions;
+		this.threadExecutor = threadExecutor;
+	}
 
 	/**
 	 * Initializes a HyperassociativeMap of the specified dimensions.
@@ -50,6 +88,7 @@ public abstract class AbstractHyperassociativeMap implements Serializable
 	public AbstractHyperassociativeMap(int dimensions)
 	{
 		this.dimensions = dimensions;
+		this.threadExecutor = new ThreadPoolExecutor(Runtime.getRuntime().availableProcessors(), Runtime.getRuntime().availableProcessors()*5, 20, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
 	}
 
 	/**
@@ -84,12 +123,29 @@ public abstract class AbstractHyperassociativeMap implements Serializable
 	 */
     public void align()
     {
+		//align all nodes in parallel
+		ArrayList<Future<Hyperpoint>> futures = new ArrayList<Future<Hyperpoint>>();
+		for(HyperassociativeNode node : this.nodes)
+			futures.add(this.threadExecutor.submit(new Align(node)));
+
+		//wait for all nodes to finish aligning and calculate new center point
 		Hyperpoint center = new Hyperpoint(this.dimensions);
-        for(HyperassociativeNode node : nodes)
+		try
 		{
-            node.align();
-			for(int dimensionIndex = 1; dimensionIndex <= this.dimensions; dimensionIndex++)
-				center.setCoordinate(center.getCoordinate(dimensionIndex) + node.getLocation().getCoordinate(dimensionIndex), dimensionIndex);
+			for(Future<Hyperpoint> future : futures)
+			{
+				Hyperpoint newPoint = future.get();
+				for(int dimensionIndex = 1; dimensionIndex <= this.dimensions; dimensionIndex++)
+					center.setCoordinate(center.getCoordinate(dimensionIndex) + newPoint.getCoordinate(dimensionIndex), dimensionIndex);
+			}
+		}
+		catch(InterruptedException caughtException)
+		{
+			throw new AssertionError("Unexpected interuption. Get should block indefinately");
+		}
+		catch(ExecutionException caughtException)
+		{
+			throw new AssertionError("Unexpected execution exception. Get should block indefinately");
 		}
 
 		for(int dimensionIndex = 1; dimensionIndex <= this.dimensions; dimensionIndex++)
