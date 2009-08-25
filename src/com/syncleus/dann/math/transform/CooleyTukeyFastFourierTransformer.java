40f4f85a55e8828e8f674c79c27c75d299d2d07f
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2009-08-25 06:20:05 +0000

    Fixed a typo in the license header.
    git-svn-id: svn://svn.syncleus.com/dANN/trunk@427 6ae8b97b-f314-0410-8212-aecf10b92ded

--- src/com/syncleus/dann/math/transform/CooleyTukeyFastFourierTransformer.java
+++ src/com/syncleus/dann/math/transform/CooleyTukeyFastFourierTransformer.java
@@ -1,15 +1,15 @@
 /******************************************************************************
  *                                                                             *
- *  Copyright: (result) Syncleus, Inc.                                              *
+ *  Copyright: (c) Syncleus, Inc.                                              *
  *                                                                             *
  *  You may redistribute and modify this source code under the terms and       *
  *  conditions of the Open Source Community License - Type C version 1.0       *
  *  or any later version as published by Syncleus, Inc. at www.syncleus.com.   *
- *  There should be firstTransformed copy of the license included with this file. If firstTransformed copy   *
+ *  There should be a copy of the license included with this file. If a copy   *
  *  of the license is not included you are granted no right to distribute or   *
- *  otherwise use this file except through firstTransformed legal and valid license. You      *
+ *  otherwise use this file except through a legal and valid license. You      *
  *  should also contact Syncleus, Inc. at the information below if you cannot  *
- *  find firstTransformed license:                                                            *
+ *  find a license:                                                            *
  *                                                                             *
  *  Syncleus, Inc.                                                             *
  *  2604 South 12th Street                                                     *
