21eb4818212a685c34833fa409d5901afe59242e
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2010-04-24 09:21:42 -0400

    Finished javadocing the Graph interface.

--- src/com/syncleus/dann/graph/Graph.java
+++ src/com/syncleus/dann/graph/Graph.java
@@ -585,8 +585,63 @@ public interface Graph<N, E extends Edge<N>>
 	 * @since 2.0
 	 */
 	boolean isMultigraph();
+
+	/**
+	 * Determines if all the nodes in the specified graph contains all the same
+	 * nodes as this graph and every pair of nodes that are adjacent in this
+	 * graph are adjacent in the specified graph, and vice versa. For
+	 * bidirected edges this means both graphs have equal nodes and the same number of edges each eith the same end points
+	 * however the traversability of the edges may differ between graphs. In
+	 * Hypergraphs there can be an entirely different number of edges and still
+	 * be isomorphic. If there are no edges and/or there are no nodes in both
+	 * graphs then they are isomorphic.
+	 *
+	 * @param isomorphicGraph graph to check if it is isomorphic with this
+	 * graph.
+	 * @return true if this graph and the specified graph have all the same
+	 * nodes and all the same pairs of adjacecy betwee them. False otherwise.
+	 * @since 2.0
+	 */
 	boolean isIsomorphic(Graph<N,E> isomorphicGraph);
+
+	/**
+	 * Determines if all the nodes in the specified graph contains all the same
+	 * nodes as this graph and every pair of nodes that are adjacent in this
+	 * graph are adjacent in the specified graph. Unlike isIsomorphic this is a
+	 * one way comparison, so while every adjacent pair of nodes in this graph
+	 * must be present int he specified graph, there may be adjacenct nodes in
+	 * the specified graph that are not adjacent in this graph. That also means
+	 * the specified graph may have nodes that dont exist in this graph, but
+	 * every node in this graph must exist int he specified graph. If there are
+	 * no edges and/or there are no nodes in both graphs then they are
+	 * homomorphic.
+	 *
+	 * @param homomorphicGraph graph to check if it is homomorphic with this
+	 * graph.
+	 * @return true if every pair of adjacent nodes in this graph are also
+	 * present and adjacent in the specified graph.
+	 * @since 2.0
+	 */
 	boolean isHomomorphic(Graph<N,E> homomorphicGraph);
+
+	/**
+	 * Determins if every node in this graph has the same degree. If there are
+	 * no nodes in the graph this will return true.
+	 *
+	 * @return true if every node in this graph has the same degree or there are
+	 * no nodes, false otherwise.
+	 * @since 2.0
+	 */
 	boolean isRegular();
+
+	/**
+	 * Determines if graph has no loops, and all edges have a multiplicity of
+	 * 0. Simple graphs can not have two nodes connected by two edges differing
+	 * only by its direction/navigability.
+	 *
+	 * @return true if graph has no loops, and all edges have a multiplicity of
+	 * 0.
+	 * @since 2.0
+	 */
 	boolean isSimple();
 }
