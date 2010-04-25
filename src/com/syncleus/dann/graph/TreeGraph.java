7cf93591982dd44d7f743b973dba84ad03b7481a
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2010-04-25 03:04:51 -0400

    Added javadocs to TreeGraph interface.

--- src/com/syncleus/dann/graph/TreeGraph.java
+++ src/com/syncleus/dann/graph/TreeGraph.java
@@ -18,8 +18,43 @@
  ******************************************************************************/
 package com.syncleus.dann.graph;
 
+/**
+ * A type of graph consisting of bidirected edges that is weakly connected,
+ * acyclic, and simple.
+ *
+ * @author Jeffrey Phillips Freeman
+ * @param <N> The class represenging a node.
+ * @param <E> The bidirected edge used to connect nodes.
+ * @since 2.0
+ */
 public interface TreeGraph<N, E extends BidirectedEdge<N>> extends BidirectedGraph<N,E>
 {
+	/**
+	 * Determines if the specified node has exactly one incomming traversable
+	 * edge, and no outgoing traversable edges. In the case of undirected edges
+	 * if the node hs a degree of exactly one it is a leaf. In the case of
+	 * directed edges a leaf has exactly one incomming edge and no other
+	 * outgoing edges. Edges whitch are not traversable in either direction are
+	 * not counted. returns false if the node has no edges or has no incomming
+	 * edges.
+	 *
+	 * @param node the node to check if it is a leaf.
+	 * @return true if the specified node has exactly one incomming traversable
+	 * edge, and no outgoing traversable edges.
+	 * @since 2.0
+	 */
 	boolean isLeaf(N node);
+
+	/**
+	 * Determines if the specified edge is traversable and has a leaf node as
+	 * its destination. The edge must exist in the graph.
+	 *
+	 * @see TreeGraph#isLeaf(java.lang.Object)
+	 * @throws IllegalArgumentException if edge does not exist in the graph.
+	 * @param edge the edge to check if it is a leaf.
+	 * @return true if the specified edge is traversable and has a leaf node as
+	 * its destination.
+	 * @since 2.0
+	 */
 	boolean isLeaf(E edge);
 }
