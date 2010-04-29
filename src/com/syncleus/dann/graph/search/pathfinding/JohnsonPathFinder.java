564f4a275d756d18e7d0af188ff44fbf6ebc741f
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2010-04-29 13:55:33 -0400

    Suppressed the final class with warnings, added TODO instead

--- src/com/syncleus/dann/graph/search/pathfinding/JohnsonPathFinder.java
+++ src/com/syncleus/dann/graph/search/pathfinding/JohnsonPathFinder.java
@@ -21,6 +21,7 @@ package com.syncleus.dann.graph.search.pathfinding;
 import com.syncleus.dann.graph.*;
 import java.util.List;
 
+@SuppressWarnings("unchecked")
 public class JohnsonPathFinder<N, E extends WeightedDirectedEdge<N>> implements PathFinder<N,WeightedDirectedEdge<N>>
 {
 	private BidirectedGraph<N,E> graph;
