05038892e38cc9f71cead98b57c86823ee5be15b
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2010-05-05 20:30:58 -0400

    removed SimpleGraph inner class fromt he Johnson Graph Transformer. It now uses the equivelant mutable graph.

--- src/com/syncleus/dann/graph/search/pathfinding/JohnsonGraphTransformer.java
+++ src/com/syncleus/dann/graph/search/pathfinding/JohnsonGraphTransformer.java
@@ -23,205 +23,6 @@ import java.util.*;
 
 public class JohnsonGraphTransformer<N> implements GraphTransformer<BidirectedGraph<N, ? extends WeightedDirectedEdge<N>>>
 {
-	private static class SimpleGraph<N> extends AbstractBidirectedGraph<N,WeightedDirectedEdge<N>>
-	{
-		private static final long serialVersionUID = 6277394757261733780L;
-		
-		private Set<N> nodes;
-
-		private final Set<WeightedDirectedEdge<N>> edges;
-		private final Map<N,Set<WeightedDirectedEdge<N>>> outEdges = new HashMap<N,Set<WeightedDirectedEdge<N>>>();
-		private final Map<N,Set<WeightedDirectedEdge<N>>> inEdges = new HashMap<N,Set<WeightedDirectedEdge<N>>>();
-
-		public SimpleGraph(Set<N> nodes, Set<WeightedDirectedEdge<N>> edges)
-		{
-			this.nodes  = nodes;
-			this.edges = edges;
-
-			for(WeightedDirectedEdge<N> edge : this.edges)
-			{
-				N outNode = edge.getSourceNode();
-				N inNode = edge.getDestinationNode();
-
-				Set<WeightedDirectedEdge<N>> outNodeEdges = this.outEdges.get(outNode);
-				if(outNodeEdges == null)
-				{
-					outNodeEdges = new HashSet<WeightedDirectedEdge<N>>();
-					this.outEdges.put(outNode, outNodeEdges);
-				}
-				Set<WeightedDirectedEdge<N>> inNodeEdges = this.inEdges.get(inNode);
-				if(inNodeEdges == null)
-				{
-					inNodeEdges = new HashSet<WeightedDirectedEdge<N>>();
-					this.inEdges.put(inNode, inNodeEdges);
-				}
-
-				outNodeEdges.add(edge);
-				inNodeEdges.add(edge);
-			}
-		}
-
-		public boolean add(WeightedDirectedEdge<N> newEdge)
-		{
-			if( newEdge == null )
-				throw new IllegalArgumentException("newEdge can not be null");
-			if( ! this.nodes.containsAll(newEdge.getNodes()) )
-				throw new IllegalArgumentException("newEdge has a node as an end point that is not part of the graph");
-
-			if( this.edges.add(newEdge) )
-			{
-				this.outEdges.get(newEdge.getSourceNode()).add(newEdge);
-				this.inEdges.get(newEdge.getDestinationNode()).add(newEdge);
-				return true;
-			}
-
-			return false;
-		}
-
-
-		public boolean add(N newNode)
-		{
-			if(newNode == null)
-				throw new IllegalArgumentException("newNode can not be null");
-
-			if( this.nodes.add(newNode) )
-			{
-				this.outEdges.put(newNode, new HashSet<WeightedDirectedEdge<N>>());
-				this.inEdges.put(newNode, new HashSet<WeightedDirectedEdge<N>>());
-				return true;
-			}
-			return false;
-		}
-
-		public boolean connect(N source, N destination, double weight)
-		{
-			if( source == null )
-				throw new IllegalArgumentException("source can not be null");
-			if( destination == null )
-				throw new IllegalArgumentException("destination can not be null");
-			if( ! this.nodes.contains( source ) )
-				throw new IllegalArgumentException("source is not a member of this graph");
-			if( ! this.nodes.contains( destination ) )
-				throw new IllegalArgumentException("destination is not a member of this graph");
-
-			return this.add(new SimpleWeightedDirectedEdge<N>(source, destination, weight));
-		}
-
-		public boolean disconnect(N source, N destination)
-		{
-			if( source == null )
-				throw new IllegalArgumentException("source can not be null");
-			if( destination == null )
-				throw new IllegalArgumentException("destination can not be null");
-			if( ! this.nodes.contains( source ) )
-				throw new IllegalArgumentException("source is not a member of this graph");
-			if( ! this.nodes.contains( destination ) )
-				throw new IllegalArgumentException("destination is not a member of this graph");
-
-			return this.remove(new SimpleWeightedDirectedEdge<N>(source, destination, 0.0));
-		}
-
-		public boolean remove(WeightedDirectedEdge<N> edgeToRemove)
-		{
-			if( edgeToRemove == null )
-				throw new IllegalArgumentException("removeSynapse can not be null");
-
-			if( this.edges.remove(edgeToRemove) )
-			{
-				if( this.outEdges.containsKey(edgeToRemove.getSourceNode()) )
-					this.outEdges.get(edgeToRemove.getSourceNode()).remove(edgeToRemove);
-				if( this.inEdges.containsKey(edgeToRemove.getDestinationNode()) )
-					this.inEdges.get(edgeToRemove.getDestinationNode()).remove(edgeToRemove);
-				return true;
-			}
-			return false;
-		}
-
-		public boolean remove(N nodeToRemove)
-		{
-			if( nodeToRemove == null )
-				throw new IllegalArgumentException("node can not be null");
-
-			if( this.nodes.remove(nodeToRemove) )
-			{
-				Set<WeightedDirectedEdge<N>> removeEdges = new HashSet<WeightedDirectedEdge<N>>();
-				if( this.outEdges.containsKey(nodeToRemove) )
-					removeEdges.addAll(this.outEdges.remove(nodeToRemove));
-				if( this.inEdges.containsKey(nodeToRemove) )
-					removeEdges.addAll(this.inEdges.remove(nodeToRemove));
-				this.edges.removeAll(removeEdges);
-
-				return true;
-			}
-			return false;
-		}
-
-		public Set<N> getNodes()
-		{
-			return Collections.unmodifiableSet(this.nodes);
-		}
-
-		@Override
-		public Set<WeightedDirectedEdge<N>> getEdges()
-		{
-			return Collections.unmodifiableSet(this.edges);
-		}
-
-		public Set<WeightedDirectedEdge<N>> getAdjacentEdges(N node)
-		{
-			Set<WeightedDirectedEdge<N>> nodeSynapses = new HashSet<WeightedDirectedEdge<N>>();
-			if( this.outEdges.containsKey(node) )
-				nodeSynapses.addAll( this.outEdges.get(node) );
-			if( this.inEdges.containsKey(node) )
-				nodeSynapses.addAll( this.inEdges.get(node) );
-			return Collections.unmodifiableSet(nodeSynapses);
-		}
-
-		@Override
-		public Set<WeightedDirectedEdge<N>> getTraversableEdges(N node)
-		{
-			if( this.outEdges.containsKey(node) )
-				 return Collections.unmodifiableSet( this.outEdges.get(node) );
-			return Collections.emptySet();
-		}
-
-		public Set<WeightedDirectedEdge<N>> getInEdges(N node)
-		{
-			if( this.inEdges.containsKey(node) )
-				 return Collections.unmodifiableSet( this.inEdges.get(node) );
-			return Collections.emptySet();
-		}
-
-		public int getIndegree(N node)
-		{
-			return this.inEdges.get(node).size();
-		}
-
-		public int getOutdegree(N node)
-		{
-			return this.outEdges.get(node).size();
-		}
-
-		public List<N> getAdjacentNodes(N node)
-		{
-			Set<WeightedDirectedEdge<N>> nodeEdges = this.getAdjacentEdges(node);
-			List<N> neighbors = new ArrayList<N>();
-			for(WeightedDirectedEdge<N> nodeEdge : nodeEdges)
-				neighbors.add( (nodeEdge.getLeftNode().equals(node) ? nodeEdge.getRightNode() : nodeEdge.getLeftNode() ) );
-			return Collections.unmodifiableList(neighbors);
-		}
-
-		@Override
-		public List<N> getTraversableNodes(N node)
-		{
-			Set<WeightedDirectedEdge<N>> adjacentOutEdges = this.getTraversableEdges(node);
-			List<N> neighbors = new ArrayList<N>();
-			for(WeightedDirectedEdge<N> outEdge : adjacentOutEdges)
-				neighbors.add( outEdge.getDestinationNode() );
-			return Collections.unmodifiableList(neighbors);
-		}
-	}
-
 	private static final Object blankNode = new Object();
 
 	private boolean containsInfinite(Graph<N,?> original)
@@ -244,16 +45,16 @@ public class JohnsonGraphTransformer<N> implements GraphTransformer<BidirectedGr
 		Set<WeightedDirectedEdge<Object>> originalEdges = new HashSet<WeightedDirectedEdge<Object>>();
 		for(WeightedDirectedEdge<N> originalEdge : original.getEdges())
 			originalEdges.add((WeightedDirectedEdge<Object>)originalEdge);
-		SimpleGraph<Object> copyGraph = new SimpleGraph<Object>(new HashSet<Object>(original.getNodes()), originalEdges );
+		SimpleMutableDirectedGraph<Object, WeightedDirectedEdge<Object>> copyGraph = new SimpleMutableDirectedGraph<Object, WeightedDirectedEdge<Object>>(new HashSet<Object>(original.getNodes()), originalEdges );
 
 		Set<Object> originalNodes = copyGraph.getNodes();
 		copyGraph.add(blankNode);
 		for(Object originalNode : originalNodes)
-			copyGraph.connect(blankNode, originalNode, 0.0);
+			copyGraph.add(new ImmutableWeightedDirectedEdge<Object>(blankNode, originalNode, 0.0));
 
 		BellmanFordPathFinder<Object, WeightedDirectedEdge<Object>> pathFinder = new BellmanFordPathFinder<Object, WeightedDirectedEdge<Object>>(copyGraph);
 
-		SimpleGraph johnsonGraph = new SimpleGraph(original.getNodes(), new HashSet<WeightedDirectedEdge<N>>(original.getEdges()));
+		SimpleMutableDirectedGraph johnsonGraph = new SimpleMutableDirectedGraph(original.getNodes(), new HashSet<WeightedDirectedEdge<N>>(original.getEdges()));
 		List<WeightedDirectedEdge<N>> edges = new ArrayList<WeightedDirectedEdge<N>>(johnsonGraph.getEdges());
 		for(WeightedDirectedEdge<N> edge : edges)
 		{
